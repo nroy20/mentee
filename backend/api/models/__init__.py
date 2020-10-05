d2feeb6934e63d734ba58db4defb1f1a14bdc61b
Leundai <batmanlup2@gmail.com>
2020-10-05 15:02:36 -0500

    Fixed Naming Issues and EmbeddedDoc

--- backend/api/models/__init__.py
+++ backend/api/models/__init__.py
@@ -4,8 +4,8 @@
 from .base import db
 from .Email import Email
 from .Person import Person
-from .VideoLinks import VideoLinks
+from .VideoInfo import VideoInfo
 
-__all__ = ["db", "Email", "Person", "VideoLinks"]
+__all__ = ["db", "Email", "Person", "VideoInfo"]
 
 # You must import all of the new Models you create to this page
